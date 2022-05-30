66f3963e9361a599e518ee408567d31c6ae2da5d
aemla <84566399+aemla@users.noreply.github.com>
2022-05-30 14:05:30 +0300

    Changed puppeteer-core to puppeteer

--- src/types/puppeteer.ts
+++ src/types/puppeteer.ts
@@ -1,4 +1,4 @@
-import type * as Puppeteer from "puppeteer-core"
+import type * as Puppeteer from "puppeteer"
 
 export type Page = Puppeteer.Page
 export type ElementHandle = Puppeteer.ElementHandle
